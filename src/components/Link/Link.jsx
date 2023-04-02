992cafd286908b09953703ff3511ff8ea33e193d
ali1924 <mohammadali229987@gmail.com>
2023-04-03 05:12:35 +0600

    53-3 Responsive Navbar using react and tailwind

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Link = (props) => {
+    // console.log(props);
+    // console.log(props.route);
+    const {id,name,path}=props.route;
+    return (
+        <li className="mr-12">
+            <a href={path}>{name}</a>
+        </li>
+    );
+};
+
+export default Link;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { BeakerIcon,Bars3Icon,XMarkIcon} from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const [open,setOpen] = useState(false);
     const routes = [
         {
             id: 1,
             name: 'Home',
-            path: '/'
+            path: '/home'
         },
         {
             id: 2,
-            name: 'About Us',
+            name: 'About',
             path: '/about'
         },
         {
             id: 3,
-            name: 'Our Services',
-            path: '/services'
+            name: 'Products',
+            path: '/products'
         },
         {
             id: 4,
-            name: 'Contact Us',
+            name: 'Contact',
             path: '/contact'
-        }
+        },
+                {
+            id: 5,
+            name: 'Services',
+            path: '/services'
+        },
     ];
 
     return (
+
         <nav>
-            {
-                routes.map(route => <li>{route.name}</li>)
-            }
+            <div onClick={() => setOpen(!open)}>
+                <span>{open===true ?'true':'false'}</span>
+                <Bars3Icon className="h-6 w-6 text-purple-500" />
+            </div>
+            <ul className="md:flex">
+                {
+                    // routes.map(route => <li>{route.name}</li>)
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
         </nav>
     );
 };
